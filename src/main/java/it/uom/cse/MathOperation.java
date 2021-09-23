b17ce497cf764b8c1bf64213f60b6e116d87b300
Supun Dhananjaya <supun.19@cse.mrt.ac.lk>
2021-09-23 11:24:51 +0530

    CSELIBRARY: sumOfPrime()

--- src/main/java/it/uom/cse/MathOperation.java
+++ src/main/java/it/uom/cse/MathOperation.java
@@ -15,4 +15,34 @@ public class MathOperation {
         return operand1 - operand2;
     }
 
+    public static int sumOfPrimes(int n){
+        int sum = 0;
+        int i = 2;
+        while(n > 0){
+            if(isPrime(i)){
+                sum += i;
+                n -= 1;
+            }
+            i++;
+        }
+        return sum;
+    }
+
+    private static boolean isPrime(int n){
+        if(n == 1){
+            return false;
+        }else if(n == 2){
+            return true;
+        }else if(n%2 == 0){
+            return false;
+        }else{
+            for(int j = 2; j < n; j++){
+                if(n%j == 0){
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
 }
